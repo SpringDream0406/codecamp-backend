de527c0c4f0d1470f30c05e93ad0500f206390d5
SpringDream0406 <SpringDream0406@gmail.com>
2024-03-22 10:49:46 +0900

    10-07 1:N 관계 등록 API

---
+++ class/section10/10-06-typeorm-crud-one-to-one/src/apis/products/product.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductSaleslocation } from '../productsSaleslocations/entities/productSaleslocation.entity';
+import { ProductsSaleslocationsService } from '../productsSaleslocations/productsSaleslocations.service';
+import { Product } from './entities/product.entity';
+import { ProductsResolver } from './product.resolver';
+import { ProductsService } from './product.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Product, //
+      ProductSaleslocation,
+    ]),
+  ],
+  providers: [
+    ProductsResolver, //
+    ProductsService,
+    ProductsSaleslocationsService,
+  ],
+})
+export class ProductsModule {}
